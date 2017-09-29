3724c968f4d6cbdb52ac309312463ad1c5c24870
cyrilforrez <cyilforrez@gmail.com>
2017-09-29 10:03:54 +0200

    add form for registration

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include 'view/app/header.php';
+include 'view/app/registration.php';
+include 'view/app/footer.php';
---
+++ view/app/registration.php
@@ -0,0 +1,33 @@
+<div class="row">
+  <form class="col s12">
+    <div class="row">
+      <div class="input-field col s6">
+        <input id="name" type="text" class="validate">
+        <label for="name">Name</label>
+      </div>
+      <div class="input-field col s6">
+        <input id="first_name" type="text" class="validate">
+        <label for="first_name">First Name</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="age" type="text" class="validate">
+        <label for="age">age</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="password" type="password" class="validate">
+        <label for="password">Password</label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="input-field col s12">
+        <input id="email" type="email" class="validate">
+        <label for="email">Email</label>
+      </div>
+    </div>
+   <input class="waves-effect waves-light btn" type="submit" name="send" value="Envoyer" >
+  </form>
+</div>
